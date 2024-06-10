f1e45bf99565206caad6e95cb9fac1c4d17cdeac
Guy Langston <guy.langston@fenics.com>
2024-06-10 10:02:42 +0100

    csharp lang features playground

---
+++ csharp-lang/Program.cs
@@ -0,0 +1,15 @@
+﻿var f = () => Console.WriteLine("a");
+f += () => Console.WriteLine("b");
+f += () => Console.WriteLine("c");
+f();
+
+Action ff = () => Console.WriteLine("1");
+// ff += Console.WriteLine("2");
+// ^^^ error CS0029: Cannot implicitly convert type 'void' to 'System.Action'
+ff();
+
+// Output:
+// a
+// b
+// c
+// 1
